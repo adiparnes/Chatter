c79067569ff173f3eae27d334b6af28eca77ca4f
niv <niv@mysizeid.com>
2020-02-22 13:08:09 +0200

    Initial Commit

---
+++ Chatter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Avihai Shabtai on 22/02/2020.
+//  Copyright © 2020 Niv-Ackerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
