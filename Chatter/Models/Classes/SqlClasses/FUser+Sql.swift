2569ac5df8ec5a37b7cf55f3e823c7df1b3c3bdb
niv <niv@mysizeid.com>
2020-03-27 01:30:40 +0200

    added sqlite and fixes bug's

---
+++ Chatter/Models/Classes/SqlClasses/FUser+Sql.swift
@@ -0,0 +1,9 @@
+//
+//  FUser+Sql.swift
+//  Chatter
+//
+//  Created by Avihai Shabtai on 25/03/2020.
+//  Copyright © 2020 Niv-Ackerman. All rights reserved.
+//
+
+import Foundation
---
+++ Chatter/Models/Classes/SqlClasses/ModelSql.swift
@@ -0,0 +1,9 @@
+//
+//  ModelSql.swift
+//  Chatter
+//
+//  Created by Avihai Shabtai on 25/03/2020.
+//  Copyright © 2020 Niv-Ackerman. All rights reserved.
+//
+
+import Foundation
